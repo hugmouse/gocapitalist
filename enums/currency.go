8c55177762bcdac32d258e77a4e4e74d077d173e
Mysh! <mysh@tuta.io>
2020-03-30 21:26:39 +0800

    [add] Ability to make a payment using float64

--- enums/currency.go
+++ enums/currency.go
@@ -12,11 +12,21 @@ const (
 	RUB Currency = "RUR"
 )
 
+// Using float for payments can be dangerous, so there are 2 choices in the structure
+//
+// Float64ToMoney - if you are 100% sure that your math is correct
+//
+// Monetary, Cents - created to be able to manage an basic monetary unit and 1/100 of it
+// Like, for example, ruble and kopeck or dollar and cent
 type Money struct {
-	Monetary int64
-	Cents    int64
+	Float64ToMoney float64
+	Monetary       int64
+	Cents          int64
 }
 
 func (c Money) String() string {
+	if c.Float64ToMoney > 0 {
+		return fmt.Sprintf("%.2f", c.Float64ToMoney)
+	}
 	return fmt.Sprintf("%d.%02d", c.Monetary, c.Cents%100)
 }
